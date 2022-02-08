1672ca2b9dad33430c89fdbcffccee14a3a644d1
vinaykukke <kukke06@gmail.com>
2022-02-08 16:17:53 +0530

    About page and profile card setup (#2)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,8 @@
-import Card from "components/Card";
 import DisplayCard from "components/DisplayCard";
 import styles from "styles/about.module.scss";
 
 const About = () => (
   <div className={styles.main}>
-    <h1>This is the about page</h1>
-    <Card
-      src="/products/instant-coffee.png"
-      specs={{
-        title: "Vinay Kukke",
-        description: "Javascript Developer",
-        price: 0,
-      }}
-    />
     <DisplayCard />
   </div>
 );
