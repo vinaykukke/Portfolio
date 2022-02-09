3b8a3b9b6c86337c179daff636cc1932683714dc
vinay kukke <kukke06@gmail.com>
2022-02-09 12:41:16 +0530

    theming for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,12 @@
 import DisplayCard from "components/DisplayCard";
+import Head from "next/head";
 import styles from "styles/about.module.scss";
 
 const About = () => (
   <div className={styles.main}>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
     <DisplayCard />
   </div>
 );
