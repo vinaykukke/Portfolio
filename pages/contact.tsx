e4ff0f0fa32b48f218b29a4c5bcf4a17eb8ac9f6
vinay kukke <kukke06@gmail.com>
2022-03-03 12:49:24 +0530

    Bug fixes

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -51,7 +51,7 @@ const Contact = () => {
   };
 
   return (
-    <section id="Contact" className="section-main__wrapper">
+    <section id="contact" className="section-main__wrapper">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
